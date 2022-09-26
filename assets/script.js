e011767d73a42490e58ebf3e0ead8dc3b8380bd1
Sabrina LeMay <b33na1997@gmail.com>
2022-09-26 11:22:54 -0700

    adjusted script.js to reflect jquery

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,46 @@
 //Current day and time
-const currentDay = document.getElementById('currentDay');
-const currentDate = moment();
-$("#currentDay").text(currentDate.format("MMM Do, YYYY, h:mm:ss"));
+const currentDay = moment().format('dddd, MMM, Do YYYY');
+$("#currentDay").html(todayDate);
 
-var containerEl = document.getElementsByClassName('container');
+$(document).ready(function (){
+    $('.saveBtn').on('click', function (){
+        var text = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
+        localStorage.setItem(time, text);
+    })
 
-const container = ["column1", "column2", "column3", "column4", "column5", "column6", "column7", "column8", "column9", "column10"];
+    function timeTracker() {
+        var currentTime = moment().hour();
 
-function submitEvent(event) {
-    event.preventDefault();
+        $('.time-block').each(function () {
+            var timeBlock = parseInt($(this).attr('id').split('hour')[1]);
 
-    var columns = $('input[name="calendar-input"]').val();
-
-    var columnEl = $();
-
-    columnEl.text(columns);
-
-    columns.append(columnEl);
-
-    $('input[name="cloumn1","column2","column3","column4","column5"]').val('');
-
-    saveBtn.on('click', function(){
-        var value = $(this)
-        .siblings('calendar-input')
-        .val();
-        var time = $(this)
-        .parent()
-        .attr('id');
-    
-        localStorage.setItem(time, value);
-        renderLastInput();
-    });
-}
-
-//saving user's inputs into planner
-var text = document.getElementsByClassName('text');
-var userInput = document.getElementsByClassName('form-input');
-var saveBtn = document.getElementsByClassName('saveBtn');
-
-renderLastInput();
-
-function displayText(type, calendarMessage){
-    text.textContent = calendarMessage
-    text.setAttribute("class", type);
-}
-
-function renderLastInput() {
-    var calendarInput = localStorage.getItem("calendar-input");
-    if (!calendarInput) {
-        return;
+            if (timeBlock < currentTime){
+                $(this).removeClass('later');
+                $(this).removeClass('now');
+                $(this).addClass('then');
+            } else if (timeBlock === currentTime){
+                $(this).removeClass('later');
+                $(this).removeClass('now');
+                $(this).addClass('then');
+            } else {
+                $(this).removeClass('later');
+                $(this).removeClass('now');
+                $(this).addClass('then');
+            }
+        })
     }
-    userInput.textContent = calendarInput;
+    $("#eightAm .description").val(localStorage.getItem("eightAm"));
+    $("#nineAm .description").val(localStorage.getItem("nineAm"));
+    $("#tenAm .description").val(localStorage.getItem("tenAm"));
+    $("#elevenAm .description").val(localStorage.getItem("elevenAm"));
+    $("#twelevePm .description").val(localStorage.getItem("twlevePm"));
+    $("#onePm .description").val(localStorage.getItem("onePm"));
+    $("#twoPm .description").val(localStorage.getItem("twoPm"));
+    $("#threePm .description").val(localStorage.getItem("threePm"));
+    $("#fourPm .description").val(localStorage.getItem("fourPm"));
+    $("#fivePm .description").val(localStorage.getItem("fivePm"));
 
-/*saveBtn.addEventlistener('click', function(){
-    var value = $(this)
-    .siblings('calendar-input')
-    .val();
-    var time = $(this)
-    .parent()
-    .attr('id');
+    timeTracker();
+})
 
-    localStorage.setItem(time, value);
-    renderLastInput();
-});*/
-}
-
-
-// make for loop for color change
-
-
-//WHEN I scroll down
-//THEN I am presented with time blocks for standard business hours
-//WHEN I view the time blocks for that day
-//THEN each time block is color-coded to indicate whether it is in the past, present, or future
-//WHEN I click into a time block
-//THEN I can enter an event
-//WHEN I click the save button for that time block
-//THEN the text for that event is saved in local storage
-//WHEN I refresh the page
